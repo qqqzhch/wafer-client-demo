9895f41ac252f2bdfcbdf5e397e114fe1ee582b5
zhangchao <zhangchao@blueocn.com.cn>
2017-11-01 13:28:10 +0800

    问卷

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,20 @@
 var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 
-Page({});
+Page({
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '吐槽你的前任原型',
+      path: 'pages/index/index',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  }
+});
